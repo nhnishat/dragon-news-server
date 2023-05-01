99042be8e330a4355964fb7787253b11ec90de3b
nhnishat <nhnishat1998@gmail.com>
2023-05-01 21:37:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+app.use(cors());
+const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
+
+app.get('/', (req, res) => {
+	res.send('dragon is running ');
+});
+
+app.get('/categories', (req, res) => {
+	res.send(categories);
+});
+
+app.get('/categories/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id == 0) {
+		res.send(news);
+	} else {
+		const categoriesNews = news.filter((n) => parseInt(n.category_id) === id);
+		res.send(categoriesNews);
+	}
+});
+
+app.get('/news', (req, res) => {
+	res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+	const id = req.params.id;
+	console.log(id);
+	const selectedNews = news.find((n) => n._id === id);
+	res.send(selectedNews);
+});
+
+app.listen(port, () => {
+	console.log(`Dragon API is Running on Port : ${port}`);
+});
